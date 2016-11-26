cd07aff101ade4fc3d052fe797e7248efad52aab
akshaykant <akshaykant1708@gmail..com>
2016-11-27 02:51:58 +0530

    Authentication

--- app/src/main/java/com/akshaykant/com/eventers/MainActivity.java
+++ app/src/main/java/com/akshaykant/com/eventers/MainActivity.java
@@ -1,13 +1,120 @@
 package com.akshaykant.com.eventers;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private static final String TAG = "MainActivity";
+
+    //Set the value RC_SIGN_IN flag used for startActivityForResult for FirebaseUI and don't use the default value.
+    private static final int RC_SIGN_IN = 1;
+
+    /*One class from Firebase Auth API*/
+    private FirebaseAuth mFirebaseAuth;
+
+    /*Event Listener that reacts to auth state change. It execute when user signs in, signs out, attached  to FriebaseAuth*/
+    // Best Practices: attach AuthStateListener in onResume() and detach in onPause()
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*Instantiate the firebase auth object*/
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+       /*Instantiate new AuthStateListener*/
+        //Attach and detach in onResume() and onPause()
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                //Check the state of the user
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // user is signed in
+                    Toast.makeText(MainActivity.this, "SIGNED IN", Toast.LENGTH_LONG).show();
+
+                } else {
+                    // user is signed out
+                    //Show the Sign In Screen
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)
+                                    .setProviders(
+                                            AuthUI.EMAIL_PROVIDER,
+                                            AuthUI.GOOGLE_PROVIDER
+                                    )
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+            }
+        };
     }
+
+    /*startActivityForResult() return onAcivityResult() with RESULT_OK or RESULT_CANCEL.
+    Here you can handle the back button pressed flow from the login page.*/
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(MainActivity.this, "Signed in!", Toast.LENGTH_LONG).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(MainActivity.this, "Sign in Cancelled!", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //attach AuthStateListener in onResume()
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //detach AuthStateListener in onPause()
+        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.sign_out_menu:
+                //sign out
+                AuthUI.getInstance().signOut(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+
 }
